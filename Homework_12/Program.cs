58ad09a6bdda972f34199620ea7694dfcba3f34f
Fu1lerene <Fu1lerene@yandex.ru>
2022-04-22 20:02:54 +0300

    Update Program.cs

--- Homework_12/Program.cs
+++ Homework_12/Program.cs
@@ -12,34 +12,17 @@ namespace Homework_12
     {
         static void Main(string[] args)
         {
-            Bank bank = new Bank("Need more gold");
+            Bank bank = new Bank("Need more gold"); /// создание банка со случайным количеством клиентов
 
-            //bank.Credit(20, 1_000_000, 24);
-            //bank.OpenDeposit(1, 500000, 6);
+            /// Сериализация
+            //string jsonString = JsonSerializer.Serialize(bank);
+            //File.WriteAllText("Need more gold.json", jsonString);
 
-            string jsonString = JsonSerializer.Serialize(bank);
-            File.WriteAllText("Need more gold.json", jsonString);
-
+            /// Десериализация
             //string json = File.ReadAllText("Need more gold.json");
             //Bank bank = JsonSerializer.Deserialize<Bank>(json);
-            //bank.PrintClients();
 
-            // Создать прототип банковской системы, позвляющей управлять клиентами и клиентскими счетами.
-            // В информационной системе есть возможность перевода денежных средств между счетами пользователей
-            // Открывать вклады, с капитализацией и без
-
-            // * Продумать возможность выдачи кредитов
-            // Продумать использование обобщений
-
-            // Продемонстрировать работу созданной системы
-
-            // Банк
-            // ├── Отдел работы с обычными клиентами
-            // ├── Отдел работы с VIP клиентами
-            // └── Отдел работы с юридическими лицами
-
-            // Дополнительно: клиентам с хорошей кредитной историей предлагать пониженую ставку по кредиту и 
-            // повышенную ставку по вкладам
+            /// Демонстрация всех реализованных методов представлена в проекте Homework_12_WPF
         }
     }
 }
